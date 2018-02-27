234d5c14945afabd62669488646b882ae48b1da5
Andrew J. Ayers <andy@ajayers.net>
2018-02-27 15:14:27 -0600

    routes under /api

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :actuals
-  resources :forecasts
-  resources :forecast_providers
-  resources :forecast_types
-  resources :farms
-  resources :farm_providers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope "api" do
+    resources :actuals
+    resources :forecasts
+    resources :forecast_providers
+    resources :forecast_types
+    resources :farms
+    resources :farm_providers
+  end
 end
