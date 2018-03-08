d95f2cba1fe268ffe0e098b3cfe140ef2c87cc16
Andrew J. Ayers <andy@ajayers.net>
2018-03-08 14:10:25 -0600

    added default not found handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: Proc.new{|e|
+    render status: 404, json: { errors: { detail: "Resource not found" } }
+  }
 end
