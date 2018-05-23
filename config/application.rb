80a4f850a9a05998e1be3220fcbd516b687cc210
Andrew J. Ayers <andy@ajayers.net>
2018-05-23 10:39:28 -0500

    always raise exception on unpermitted parameters

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,9 @@ module Windview
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # always raise exception on unpermitted parameters
+    config.action_controller.action_on_unpermitted_parameters = :raise
+
   end
 end
