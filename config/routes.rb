51d6686e581b0be18fcc937bfd7cde2a6c0397da
Andrew J. Ayers <andy@ajayers.net>
2018-05-24 11:00:28 -0500

    code cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   get '/apidocs' => redirect('/swagger/dist/index.html?url=/openapi.json')
   scope "api" do
-    resources :actuals
+
+    resources :farm_providers
+    resources :farms
+
+    resources :forecast_providers
+    resources :forecast_types
 
     get '/forecasts/latest', to: 'forecasts#latest'
     resources :forecasts
-    resources :forecast_providers
-    resources :forecast_types
-    resources :farms
-    resources :farm_providers
+    resources :actuals
   end
 end
